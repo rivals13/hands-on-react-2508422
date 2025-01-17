6f7ee80ce039b8d4fe288980b060ee94084c5e3a
rivals13 <145206194+rivals13@users.noreply.github.com>
2025-01-17 04:44:18 +0000

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 function App() {
+  const fucnt=()=>{
+    let  i= 32
+console.log(i)
+  }
+  //  anonymous  fucntion 
+  // arrow  function 
+  //mapping the array
+  
   return (
     <hgroup>
       <h1>Meet the StarGazers</h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+   
+   <button onClick={()=>{
+return(
+  <h1> hello  world!!! </h1>
+)
+   }}></button>
     </hgroup>
+     
   )
 }
 export default App
